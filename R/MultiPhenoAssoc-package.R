6570f0ddab8d0b77b789bf10b272dc8e62ccea63
rick.chang <rick.chang@ailabs.tw>
2023-02-09 22:33:56 -0500

    import export fn

--- R/MultiPhenoAssoc-package.R
+++ R/MultiPhenoAssoc-package.R
@@ -1,6 +1,21 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
+#' @title Multivariate phenotype-gene association analysis
+#'
+#' @description
+#' XXXXXX
+#'
+#' @name MultiPhenoAssoc-package
+#'
+#' @docType package
+#'
+#' @import utils
+#' @import dplyr
+#' @import survival
+#' @import parallel
+#' @import Rfast
+#' @import stringr
+#' @import pbmcapply
+#'
+#' @importFrom stats glm lm residuals sd var
+#' @importFrom utils setTxtProgressBar txtProgressBar
+#'
 NULL
